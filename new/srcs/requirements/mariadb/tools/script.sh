4e4efa4a20d92440bd99a0f79c11e2831b8b1864
Jan Sanger <jsanger@2-C-3.42heilbronn.de>
2024-09-19 01:53:15 +0200

    added mariadb made some changes for simplification

---
+++ new/srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# wait until Mariadb is fully stated
+until mysqladmin ping --silent; do
+    echo "Warte auf MariaDB..."
+    sleep 2
+done
+
+# creating database and the user
+mysql -e "CREATE DATABASE IF NOT EXISTS $m_db_name;"
+mysql -e "CREATE USER IF NOT EXISTS '$m_db_user'@'%' IDENTIFIED BY '$m_db_pwd';"
+mysql -e "GRANT ALL PRIVILEGES ON $m_db_name.* TO '$m_db_user'@'%';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345';"
+mysql -e "FLUSH PRIVILEGES;"
+
+# start mariadb and set it to foreground
+exec mysqld
