280de5a329f006763a2ca5dcc78e07540e05aeae
Jan Sanger <jsanger@2-C-3.42heilbronn.de>
2024-09-19 02:16:24 +0200

    added nginx with all needed files, changed dockerfile for using the script if i need to add things

---
+++ new/srcs/requirements/nginx/tools/script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# (its the entrypoint.sh)
+
+# can create the ssl_certificate with open ssl here, but for simplification
+# i just createt them only once and have them in the conf/ssl directory
+
+# start the main procces
+nginx -g 'daemon off;' 
