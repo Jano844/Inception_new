255fb55a75b08b2ee81d14909244432c6fae2ed5
Jan Sanger <jsanger@2-C-5.42heilbronn.de>
2024-09-24 21:25:31 +0200

    mariadb to bullseye, starts correctly with docker build docker run -it

--- new/srcs/requirements/mariadb/tools/script.sh
+++ new/srcs/requirements/mariadb/tools/script.sh
@@ -31,17 +31,34 @@
 #!/bin/sh
 
 # Start the MariaDB server in the background
-/usr/bin/mysqld --skip-networking &
-sleep 5
+service mariadb start
 
-echo "CREATE DATABASE IF NOT EXISTS $m_db_name ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$m_db_user'@'%' IDENTIFIED BY '$m_db_pwd' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $m_db_name.* TO '$m_db_user'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+until mysqladmin ping &>/dev/null; do
+	echo "wait for Mariadb"
+	sleep 1
+done
+
+
+echo "MariaDB is started."
+
+# echo "CREATE DATABASE IF NOT EXISTS $m_db_name ;" > db1.sql
+# echo "CREATE USER IF NOT EXISTS '$m_db_user'@'%' IDENTIFIED BY '$m_db_pwd' ;" >> db1.sql
+# echo "GRANT ALL PRIVILEGES ON $m_db_name.* TO '$m_db_user'@'%' ;" >> db1.sql
+# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+# echo "FLUSH PRIVILEGES;" >> db1.sql
+
+echo "CREATE DATABASE IF NOT EXISTS \`mariadb_name\` ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS 'mariadb_user'@'%' IDENTIFIED BY 'mariadb_password' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON \`mariadb_name\`.* TO 'mariadb_user'@'%' ;" >> db1.sql
+# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
 mysql < db1.sql
 
-kill $(cat /var/run/mysqld/mysqld.pid)
+sleep 2
 
-exec mysqld
+service mariadb stop
+
+sleep 2
+
+exec mariadbd --user=mysql
